1861d52bf41c1de04d253cb8a1840354436b0f96
Harsh <harshprajapati0139@gmail.com>
2025-09-08 19:19:15 +0530

    Service Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Services from './Pages/Services'
+import Portfolio from './Pages/Portfolio'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme/theme'
 function App() {
 
 
   return (
     <>
+    <ThemeProvider theme={theme}>
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/portfolio' element={<Portfolio/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
      
+
     </>
   )
 }
